a5a078c591fbbe885136a088bb67f170a346a38c
Matheus Reis <matheus.reis081@gmail.com>
2023-03-17 11:58:38 -0300

    hello cargo

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("\nHello, cargo!\n
+    Cargo is a build system and package manager.\r
+    Generating a new project using Cargo is simple, you need using the command: 'cargo new name_project --bin'\r
+    Compiling: 'cargo build'\r 
+    Run: './target/debug/name_project'\r
+    Compiling and run: 'cargo run'\r
+    Check if it will compile correctly: 'cargo check'");
+}
